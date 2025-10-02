276cdb89052d8f16a636fec4123733afdee27a82
Anish Raina <araina2@scu.edu>
2025-10-01 18:38:49 -0700

    My edit

--- backend/server.py
+++ backend/server.py
@@ -9,9 +9,11 @@ CORS(app)
 @app.route('/')
 def hello_world():
     """
-    Hello World test endpoint
+    Hello World test endpoints 
     """
     return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
+
+# Hello World - Anish 
