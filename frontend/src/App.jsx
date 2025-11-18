ea1cb3a2e7c80645f54d07a79f80295051fbb299
Anish Raina <anishraina2@gmail.com>
2025-11-17 16:00:02 -0800

    Changed Page Title to match current page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Main from "./pages/Main";
 import FAQ from "./pages/FAQ";
 import GameDetail from "./pages/GameDetail";
 import Favorites from "./pages/Favorites";
 import Navbar from "./components/Navbar";
 
+function TitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const pathToTitle = {
+      '/': 'Home',
+      '/favorites': 'Favorite Teams',
+      '/FAQ': 'FAQ'
+    };
+
+    if (location.pathname.startsWith('/game/')) {
+      document.title = 'Game Details - Rookie Play';
+    } else {
+      const title = pathToTitle[location.pathname] || 'Rookie Play';
+      document.title = `${title} - Rookie Play`;
+    }
+  }, [location]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <div className="min-h-screen">
       <BrowserRouter>
+        <TitleUpdater />
         <Navbar />
         <Routes>
           <Route path="/" element={<Main />} />
@@ -20,3 +43,4 @@ export default function App() {
     </div>
   );
 }
+
